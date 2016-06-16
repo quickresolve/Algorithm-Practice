c2ad323d9284e178e0a4a1bf2dab0209213e9327
QuickResolve <lauranmontoya@gmail.com>
2016-06-15 18:27:52 -0700

    complete ex9 with comments

---
+++ PythonTheHardWay/ex9.py
@@ -0,0 +1,14 @@
+#Printing, Printing, Printing
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJune\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+With the three double-quotesself.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6self.
+"""
