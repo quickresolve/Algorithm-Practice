c380ab5e277fa228c13a8a392a19ad92f8c95891
QuickResolve <lauranmontoya@gmail.com>
2016-06-15 18:00:34 -0700

    finish ex7 with comments

---
+++ PythonTheHardWay/ex7.py
@@ -0,0 +1,28 @@
+#More Printing
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10 #what'd that do?
+
+end1 = "C"
+end2 = 'h'
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end. try removing it to see what happends
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+
+#writing a line of code in python that is longer then 80 characters is bad style
+#use a , comma like in the example above to wrap to the next line of code
+#if you remove the comma in the last example, the final line will print on it's own line instead of next to the one before it
