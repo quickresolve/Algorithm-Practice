ce7dd563492031b7d3f511af70b3d6b9080bd469
QuickResolve <lauranmontoya@gmail.com>
2016-06-15 17:15:46 -0700

    add comments to ex4

--- PythonTheHardWay/ex4.py
+++ PythonTheHardWay/ex4.py
@@ -13,5 +13,10 @@ print "There are", cars, "cars available."
 print "There are only", drivers, "drivers available."
 print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity, "people today."
+#A float multiplied by a whole number will produce a float.
 print "We have", passengers, "to carpool today."
 print "We need to put about", average_passengers_per_car, "in each car."
+
+
+#To pring without spaces between words use modulus %
+#ex: print "Hey %s there." % "you"
