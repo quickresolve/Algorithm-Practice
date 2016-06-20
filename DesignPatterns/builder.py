38a1b12d3cf82e82a0507a9e46e79d189e1de6c5
QuickResolve <lauranmontoya@gmail.com>
2016-06-19 17:25:47 -0700

    complete builder

--- DesignPatterns/builder.py
+++ DesignPatterns/builder.py
@@ -4,9 +4,13 @@ class Director():
         self._builder = builder
 
     def construct_car(self):
+        self._builder.create_new_car()
+        self._builder.add_model()
+        self._builder.add_tires()
+        self._builder.add_engine()
 
     def get_car(self):
-
+        return self._builder.car
 
 
 class Builder():
@@ -27,6 +31,9 @@ class SkylarkBuilder(Builder):
     def add_tires(self):
         self.car.tires = "Regular tires"
 
+    def add_engine(self):
+        self.car.engine = "Turbo engine"
+
 
 class Car():
     """Product"""
@@ -37,3 +44,11 @@ class Car():
 
     def __str__(self):
         return '{} | {} | {}'.format(self.model, self.tires, self.engine)
+
+
+builder = SkylarkBuilder()
+director = Director(builder)
+director.construct_car()
+car = director.get_car()
+
+print(car)
